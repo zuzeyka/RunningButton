be82dda338d4adafe6dfbfcb50b69d38fba882ad
zuzeyka <stepan.tarasenko.26@gmail.com>
2022-09-01 12:47:52 +0300

    Добавьте файлы проекта.

---
+++ RunningButton/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunningButton
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.X >= button1.Location.X && e.X <= button1.Location.X + button1.Width || e.Y >= button1.Location.Y && e.Y <= button1.Location.Y + button1.Width)
+            {
+                button1.Location = new Point(random.Next(0, this.Size.Width), random.Next(0, this.Size.Height));
+            }
+        }
+    }
+}
